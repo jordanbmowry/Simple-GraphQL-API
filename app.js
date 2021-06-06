74d0fcbcddcb450d67f31587c8a19082de04b3d5
jordanbmowry <jordan.mowry@gmail.com>
2021-06-06 11:21:02 -0700

    created schema.js. passed in options in app.js into graphHTTP object. enabled graphiql.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const schema = require('./schema.js');
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Test');
-});
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		graphiql: true,
+	})
+);
 
 app.listen(3000, () => {
 	console.log('App listening on port 3000');
